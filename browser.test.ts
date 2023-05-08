2a39bb2ef686852fc9574ea94c02f82368ac10e7
NicolaSpadari <spadari.nicola@gmail.com>
2023-05-08 14:48:24 +0200

    Add a variable json in order to avoid touching the test itself

--- browser.test.ts
+++ browser.test.ts
@@ -1,6 +1,7 @@
 import { test } from "@playwright/test";
+import { url } from "./variables.json";
 
 test("test browser", async ({ page }) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(url);
     await page.pause();
 });
